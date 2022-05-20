adfb20b8f7b57a2e874fdd07acf42d020ff64122
Alexis <geiner.salcedo.ppc0111@gmail.com>
2022-05-19 21:33:15 -0500

    feat: theming

---
+++ src/styles/main.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    background-color: ${props => props.theme.backgorundColor};
+`;
---
+++ src/styles/theme.styled.jsx
@@ -0,0 +1,5 @@
+
+export const theme = {
+    backgorundColor: '#f5f5f5',
+    main: "mediumseagreen"
+  };
