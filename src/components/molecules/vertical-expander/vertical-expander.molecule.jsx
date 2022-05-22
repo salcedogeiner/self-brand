b4b0f781d3c33b84e533a281a92e956ba766280f
Alexis <geiner.salcedo.ppc0111@gmail.com>
2022-05-22 18:39:45 -0500

    feat: fix icon

--- src/components/molecules/vertical-expander/vertical-expander.molecule.jsx
+++ src/components/molecules/vertical-expander/vertical-expander.molecule.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Expander } from './expander.styled.jsx';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const VerticalExpander = (props) => {
     return (
         <Expander {...props}>
-             <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+             <FontAwesomeIcon icon={faAnglesRight}></FontAwesomeIcon>
         </Expander>
     )
 }
